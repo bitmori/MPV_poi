b48909b7906d73ea5bd564e40f01daf796b8cc11
Kirk Young <neonmori@gmail.com>
2016-01-11 23:56:05 -0500

    Initial Commit

---
+++ mpv_poi/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  mpv_poi
+//
+//  Created by Kirk Young on 1/11/16.
+//  Copyright © 2016 Kirk Young. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
---
+++ mpv_poi/main.m
@@ -0,0 +1,13 @@
+//
+//  main.m
+//  mpv_poi
+//
+//  Created by Kirk Young on 1/11/16.
+//  Copyright © 2016 Kirk Young. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[]) {
+    return NSApplicationMain(argc, argv);
+}
